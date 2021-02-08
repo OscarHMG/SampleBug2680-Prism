295bc929aec9b95cd1d01357867f6ce443876eff
pabloeav <p.arredondov91@gmail.com>
2021-02-08 13:58:08 -0500

    Agrega soporte para sqlite

---
+++ ProyectoBaseXF/ProyectoBaseXF/Data/PBDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ProyectoBaseXF.Data
+{
+    public class PBDbContext : DbContext
+    {
+        
+        private const string databaseName = "pb.db";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            String databasePath = "";
+            databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), databaseName);
+            // Specify that we will use sqlite and the path of the database here
+            optionsBuilder.UseSqlite($"Filename={databasePath}");
+        }
+
+    }
+}
---
+++ ProyectoBaseXF/ProyectoBaseXF/Service/DatabaseHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoBaseXF.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoBaseXF.Service
+{
+    public class DatabaseHelper<T> where T : PBDbContext
+    {
+        protected PBDbContext CrateContext()
+        {
+            PBDbContext context = (T)Activator.CreateInstance(typeof(T));
+            //rfsContext.Database.EnsureCreated();
+            try
+            {
+                context.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return context;
+        }
+
+        public void DeleteDatabase()
+        {
+            using PBDbContext context = (T)Activator.CreateInstance(typeof(T));
+            context.Database.EnsureDeleted();
+        }
+
+    }
+}
